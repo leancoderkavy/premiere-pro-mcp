e2e64cf8ed834933891d67e198eaa4290c14e207
leancoderk <7378246+leancoderkavy@users.noreply.github.com>
2026-07-26 00:45:23 -0700

    Add safe recovery diagnostics and accessible bridge status (#51)

--- src/server.ts
+++ src/server.ts
@@ -29,6 +29,7 @@ import { getCaptionTools } from "./tools/captions.js";
 import { getPlaybackTools } from "./tools/playback.js";
 import { getProjectManagerTools } from "./tools/project-manager.js";
 import { getEditPlanTools } from "./tools/edit-plans.js";
+import { getRecoveryTools } from "./tools/recovery.js";
 import { getAvSettingsTools } from "./tools/av-settings.js";
 import { guardToolHandler, resolveCapabilities } from "./security/index.js";
 import { EXTENDSCRIPT_REFERENCE } from "./resources/extendscript-reference.js";
@@ -257,6 +258,7 @@ function collectTools(
     ...getPlaybackTools(bridgeOptions),
     ...getProjectManagerTools(bridgeOptions),
     ...getEditPlanTools(bridgeOptions, { capabilities }),
+    ...getRecoveryTools(bridgeOptions),
     ...getAvSettingsTools(bridgeOptions),
   };
   Object.assign(
